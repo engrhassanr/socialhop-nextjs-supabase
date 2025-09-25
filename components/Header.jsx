22ece9790313afdfe5711070d45984f309f0af81
Sydney Fesser <info@developaway.de>
2025-09-25 16:34:50 +0500

    refactor(middleware, header): streamline public routes in middleware and enhance header component with conditional user authentication

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,16 @@
+"use client";
 import React from "react";
 import css from "@/styles/Header.module.css";
-import { Flex } from "antd";
+import { Flex, Button } from "antd";
 import Image from "next/image";
-import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { UserButton, useUser } from "@clerk/nextjs";
 import Box from "./Box";
 import ModeButton from "./ModeButton";
 import SidebarButton from "./SidebarButton";
 const Header = () => {
+  const { isSignedIn, isLoaded } = useUser();
+
   return (
     <header className={css.wrapper}>
       <Box style={{ height: "100%" }}>
@@ -29,7 +33,19 @@ const Header = () => {
           {/* actions */}
           <Flex gap={25} align="center">
             <ModeButton />
-            <UserButton afterSignOutUrl="/sign-in" />
+            {isLoaded && (
+              <>
+                {isSignedIn ? (
+                  <UserButton afterSignOutUrl="/sign-in" />
+                ) : (
+                  <Link href="/sign-in">
+                    <Button type="primary" size="large">
+                      Sign In
+                    </Button>
+                  </Link>
+                )}
+              </>
+            )}
           </Flex>
         </div>
       </Box>
